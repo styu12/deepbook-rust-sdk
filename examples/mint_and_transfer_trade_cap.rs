385b29f83d7f3d4a677cdf6f85349eed786d3bb1
Jarry <jarry@daangn.com>
2024-12-25 20:04:18 +0900

    chore: minor refactoring

--- examples/mint_and_transfer_trade_cap.rs
+++ examples/mint_and_transfer_trade_cap.rs
@@ -2,17 +2,11 @@
 
 use std::collections::HashMap;
 use std::sync::Arc;
-use shared_crypto::intent::Intent;
-use sui_config::{sui_config_dir, SUI_KEYSTORE_FILENAME};
-use sui_keys::keystore::{AccountKeystore, FileBasedKeystore};
-use sui_sdk::rpc_types::SuiTransactionBlockResponseOptions;
 use sui_sdk::SUI_COIN_TYPE;
 use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
-use sui_types::quorum_driver_types::ExecuteTransactionRequestType;
-use sui_types::transaction::{Transaction, TransactionData};
 use deepbook::{DeepBookClient, DeepBookConfig};
 use deepbook::utils::constants::{BalanceManager, BalanceManagerMap};
-use crate::utils::get_all_coins;
+use crate::utils::{execute_transaction_block, get_all_coins};
 
 mod utils;
 
@@ -50,8 +44,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Arc::new(db_config),
     );
 
+    // Step 5: Add mint_and_transfer_trade_cap transaction to PTB with deepbook-sdk
     let mut ptb = ProgrammableTransactionBuilder::new();
-
     match db_client.mint_and_transfer_trade_cap(
         &mut ptb,
         "MANAGER_1",
@@ -65,40 +59,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let pt = ptb.finish();
-    println!("pt: {:?}", pt);
-
-    // tx_data
-    let gas_price = sui.read_api().get_reference_gas_price().await?;
-    let tx_data = TransactionData::new_programmable(
-        sender,
-        coins
-            .iter()
-            .map(|coin| coin.object_ref())
-            .collect::<Vec<_>>(),
-        pt,
-        10_000_000, // gas_budget (0.01 SUI)
-        gas_price,
-    );
-
-    // 4) sign transaction
-    let keystore = FileBasedKeystore::new(&sui_config_dir()?.join(SUI_KEYSTORE_FILENAME))?;
-    let signature = keystore.sign_secure(&sender, &tx_data, Intent::sui_transaction())?;
-
-    // execute the transaction
-    println!("Executing the transaction...");
-    let transaction_response = sui
-        .quorum_driver_api()
-        .execute_transaction_block(
-            Transaction::from_data(tx_data, vec![signature]),
-            SuiTransactionBlockResponseOptions::full_content(),
-            Some(ExecuteTransactionRequestType::WaitForLocalExecution),
-        )
-        .await?;
-
-    println!("transaction hash:\n {:?}\n", transaction_response.digest.to_string());
-    println!("transaction effect:\n {:?}\n", transaction_response.effects);
-    println!("transcation object changes:\n {:?}\n", transaction_response.object_changes);
+    // Step 6: Execute the transaction block
+    if let Err(e) = execute_transaction_block(&sui, ptb, sender).await {
+        println!("Error executing transaction block for 'mint_and_transfer_trade_cap'");
+        for source in e.chain() {
+            println!("Caused by: {}", source);
+        }
+    }
 
     Ok(())
 }
